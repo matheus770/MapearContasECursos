12c9a9808beba898e53826c7d41aa9fddacf19f5
NecoMatheus <matheus770@users.noreply.github.com>
2022-06-30 23:23:43 -0300

    adição de menu de opções

--- main.py
+++ main.py
@@ -1,37 +1,46 @@
 from fileinput import close
 import json, os.path
+while True:
+    accounts = []
 
-accounts = []
+    print("Obs: Em cursos separe-os por virgulas ao inves de espaços...Ex: matematica aplicada,portugues pra leigos...")
+    sitename = str(input("Digite o nome do site/empresa: "))
+    email = str(input("Digite o email registrado:"))
+    cursos = str(input("Digite o nome dos cursos:"))
+    scursos = cursos.split(",")
+    logingoogle = str(input("Login com o google: "))
 
-print("Obs: Em cursos separe-os por virgulas ao inves de espaços...Ex: matematica aplicada,portugues pra leigos...")
-sitename = str(input("Digite o nome do site/empresa: "))
-email = str(input("Digite o email registrado:"))
-cursos = str(input("Digite o nome dos cursos:"))
-scursos = cursos.split(",")
-logingoogle = str(input("Login com o google: "))
+    dictio = {
+        "sitename": sitename,
+        "email": email,
+        "cursos": scursos,
+        "logingoogle": logingoogle,
+    }
 
-dictio = {
-    "sitename": sitename,
-    "email": email,
-    "cursos": scursos,
-    "logingoogle": logingoogle,
-}
-
-if(os.path.isfile('dados.json')):
-    jdictio = json.dumps(dictio, indent=4)
-    with open("dados.json", "r") as json_file:
-        dados = json.load(json_file)
-        dados.append(dictio)
-        close
-    with open("dados.json", "w") as json_file:
-        jdictio = json.dumps(dados, indent=4)
-        json_file.write(jdictio)
-        close
-else:
-    accounts.append(dictio)
-    jdictio = json.dumps(accounts, indent=4)
-    with open("dados.json", "w") as json_file:
-        json_file.write(jdictio)
-        close
+    if(os.path.isfile('dados.json')):              
+        jdictio = json.dumps(dictio, indent=4)
+        with open("dados.json", "r") as json_file:
+            dados = json.load(json_file)
+            dados.append(dictio)
+            close
+        with open("dados.json", "w") as json_file:
+            jdictio = json.dumps(dados, indent=4)
+            json_file.write(jdictio)
+            close
+        print("O que deseja fazer 1 == Registrar outra conta || 2 == Exportar para PDF || 3 == Sair")
+        
+        op = int(input("Qual a opção desejada: "))
+        if op == 1:
+            continue
+        elif op == 2:
+            pass
+        elif op == 3:
+            break
+    else:
+        accounts.append(dictio)
+        jdictio = json.dumps(accounts, indent=4)
+        with open("dados.json", "w") as json_file:
+            json_file.write(jdictio)
+            close
 
 
