5ec1ff4d794f6498bcd36b2c0cc29fc24703e7ff
NecoMatheus <matheus770@users.noreply.github.com>
2022-07-03 16:51:33 -0300

    Finalizando

--- main.py
+++ main.py
@@ -2,51 +2,52 @@ from fileinput import close
 import json, os.path
 from model import ContaCurso
 
-
-while True:
-    accounts = []
-
-    print("Obs: Em cursos separe-os por virgulas ao inves de espaços...Ex: matematica aplicada,portugues pra leigos...")
-    sitename = str(input("Digite o nome do site/empresa: "))
-    email = str(input("Digite o email registrado:"))
-    cursos = str(input("Digite o nome dos cursos:"))
-    scursos = cursos.split(",")
-    logingoogle = str(input("Login com o google: "))
-
-    dadosuser = ContaCurso(sitename,email,scursos,logingoogle)
-    dadosdic = dadosuser.to_Json()
-    accounts.append(dadosdic)
-    dictio = accounts
+while(True):
     
-    if(os.path.isfile('dados.json')):              
-        jdictio = json.dumps(dictio, indent=4)
-        with open("dados.json", "r") as json_file:
-            dados = json.load(json_file)
-            dados.append(dictio)
-            close
-        with open("dados.json", "w") as json_file:
-            jdictio = json.dumps(dados, indent=4)
-            json_file.write(jdictio)
-            close
-        with open('dados.txt','a',newline='') as arquivo:
-            arquivo.write(dadosuser.show())
-            arquivo.write('\n')
-            close
-        print("O que deseja fazer 1 == Registrar outra conta || 2 == Sair ")
-        op = int(input("Qual a opção desejada: "))
-        if op == 1:
-            continue
-        elif op == 2:
-            break
-    else:
-        jdictio = json.dumps(accounts, indent=4)
-        with open("dados.json", "w") as json_file:
-            json_file.write(jdictio)
-            close
-        with open('dados.txt','a',newline='') as arquivo:
-            arquivo.write(dadosuser.show())
-            arquivo.write('\n')
-            close
+    print("O que deseja fazer 1 == Registrar Dados || 2 == Sair: ")
+    opcao = input("Qual a opção desejada: ")
+    if opcao == str(1):
+        accounts = []
+
+        print("Obs: Em cursos separe-os por virgulas ao inves de espaços...Ex: matematica aplicada,portugues pra leigos...")
+        sitename = str(input("Digite o nome do site/empresa: "))
+        email = str(input("Digite o email registrado:"))
+        cursos = str(input("Digite o nome dos cursos:"))
+        splitcursos = cursos.split(",") #! É gerado um split separando os cursos pelo caractere "," e armazenado em um array com os nome dos cursos separados.
+        logingoogle = str(input("Login com o google: "))
+
+        dadoscontacurso = ContaCurso(sitename,email,splitcursos,logingoogle)
+        dadosdict = dadoscontacurso.to_Json()
+        
+        if(os.path.isfile('dados.json')):              
+            dadosjson = json.dumps(dadosdict, indent=4)
+            with open("dados.json", "r") as json_file:
+                dados = json.load(json_file)
+                dados.append(dadosdict)
+                close
+            with open("dados.json", "w") as json_file:
+                dadosjson = json.dumps(dados, indent=4)
+                json_file.write(dadosjson)
+                close
+            with open('dados.txt','a',newline='') as arquivo:
+                arquivo.write(dadoscontacurso.show())
+                arquivo.write('\n')
+                close
+        else:
+            accounts.append(dadosdict)
+            dadosjson = json.dumps(accounts, indent=4)
+            with open("dados.json", "w") as json_file:
+                json_file.write(dadosjson)
+                close
+            with open('dados.txt','a',newline='') as arquivo:
+                arquivo.write(dadoscontacurso.show())
+                arquivo.write('\n')
+                close
+    elif opcao == str(2):
         break
+    elif (opcao != 1) or (opcao != 2) or (type(opcao) == str):
+        print("Digite uma opção valida!!!")
+        continue
+    
 
 
